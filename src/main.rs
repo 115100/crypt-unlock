c25d6d783e716bd5c56f868cdff0529510b55e6a
115100 <115100@users.noreply.github.com>
2019-08-13 22:18:14 +0100

    main: Clean up imports

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 extern crate libc;
 extern crate nix;
 
-use std::ffi;
+use nix::sys::termios;
+use std::ffi::CString;
 use std::fs::File;
-use std::io;
-use std::io::Write;
+use std::io::{stdin, stdout, Write};
 use std::os::unix::io::AsRawFd;
 use std::ptr::write_volatile;
-use std::string;
+use std::string::String;
 
 // MAX_PASSPHRASE_LEN is required to preallocate
 // String such that it will never be copied.
@@ -20,22 +20,22 @@ fn main() -> Result<(), Box<std::error::Error>> {
     // Adopted from the description in:
     // http://man7.org/linux/man-pages/man3/getpass.3.html.
     let tty = File::open("/dev/tty")?;
-    let oflags = nix::sys::termios::tcgetattr(tty.as_raw_fd())?;
+    let oflags = termios::tcgetattr(tty.as_raw_fd())?;
     let mut nflags = oflags.clone();
-    nflags.local_flags &= !nix::sys::termios::LocalFlags::ECHO;
-    nflags.local_flags |= nix::sys::termios::LocalFlags::ECHONL;
-    nix::sys::termios::tcsetattr(tty.as_raw_fd(), nix::sys::termios::SetArg::TCSANOW, &nflags)?;
+    nflags.local_flags &= !termios::LocalFlags::ECHO;
+    nflags.local_flags |= termios::LocalFlags::ECHONL;
+    termios::tcsetattr(tty.as_raw_fd(), termios::SetArg::TCSANOW, &nflags)?;
 
     // Get the passphrase
     print!("Enter your passphrase: ");
-    io::stdout().flush()?;
+    stdout().flush()?;
     // Avoid reallocations so we can zero out reliably.
-    let mut passphrase = string::String::with_capacity(MAX_PASSPHRASE_LEN + 1);
-    io::stdin().read_line(&mut passphrase)?;
-    let passphrase = ffi::CString::new(passphrase)?;
+    let mut passphrase = String::with_capacity(MAX_PASSPHRASE_LEN + 1);
+    stdin().read_line(&mut passphrase)?;
+    let passphrase = CString::new(passphrase)?;
 
     // Restore echo.
-    nix::sys::termios::tcsetattr(tty.as_raw_fd(), nix::sys::termios::SetArg::TCSANOW, &oflags)?;
+    termios::tcsetattr(tty.as_raw_fd(), termios::SetArg::TCSANOW, &oflags)?;
 
     // Now, write passphrase into /dev/console.
     {
